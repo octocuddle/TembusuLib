1dd47c9d265da5728f1b982f4d6d3faf3f75c89b
octocuddle <hanjing.cp@gmail.com>
2025-10-20 19:48:16 +0800

    Removed the dependency on Dialogflow

--- conversation_module/__init__.py
+++ conversation_module/__init__.py
@@ -1,17 +1,17 @@
 # conversation_module/__init__.py
-from .lex_handler import LexHandler
-from .dialogflow_handler import DialogflowHandler
+# from .lex_handler import LexHandler
+# from .dialogflow_handler import DialogflowHandler
 from .custom_handler import CustomHandler 
 
 def get_conversation_handler(service_provider: str):
     """
     Factory function to return the appropriate chatbot service based on the provider.
     """
-    if service_provider == "lex":
-        return LexHandler()
-    elif service_provider == "dialogflow":
-        return DialogflowHandler()
-    elif service_provider == "custom":
+    #if service_provider == "lex":
+    #    return LexHandler()
+    #elif service_provider == "dialogflow":
+    #    return DialogflowHandler()
+    if service_provider == "custom":
         return CustomHandler()
     else:
         raise ValueError(f"Unsupported service provider: {service_provider}")
